818806b160af7cd63df826db6ba5721bcb0890c5
Francisco Nuñez <fonfonferi@gmail.com>
2024-04-24 20:17:32 -0700

    BaseEntity set true as default value to IsActive #3

--- src/Core/Fnunez.TiendaExpress.Domain/Common/BaseEntity.cs
+++ src/Core/Fnunez.TiendaExpress.Domain/Common/BaseEntity.cs
@@ -3,5 +3,5 @@ namespace Fnunez.TiendaExpress.Domain.Common;
 public abstract class BaseEntity
 {
     public string? Id { get; set; }
-    public bool IsActive { get; set; }
+    public bool IsActive { get; set; } = true;
 }
